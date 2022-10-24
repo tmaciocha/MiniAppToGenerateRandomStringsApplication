7bf08b1a8f3d986838a6c47ede5c099de561fe3e
Tomasz Maciocha <tmaciocha@gmail.com>
2022-10-24 13:35:12 +0200

    Beggining

---
+++ src/main/java/com/example/miniapptogeneraterandomstrings/MiniAppToGenerateRandomStringsApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniapptogeneraterandomstrings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAppToGenerateRandomStringsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniAppToGenerateRandomStringsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniapptogeneraterandomstrings/MiniAppToGenerateRandomStringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniapptogeneraterandomstrings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAppToGenerateRandomStringsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
